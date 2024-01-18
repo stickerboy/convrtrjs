3109810f409ce6b0868cf33ebd8b05be423f1a3c
Kenny <stickerboy@users.noreply.github.com>
2024-01-18 12:14:43 +0000

    Further split of JS assets
    Core: included on all pages
    Other folders are for each page
    Updated config to match structure

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,5 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/css/*.css");
     eleventyConfig.addPassthroughCopy("assets/favicons/*");
     eleventyConfig.addPassthroughCopy("assets/img/*");
-    eleventyConfig.addPassthroughCopy("assets/js/*.js");
+    eleventyConfig.addPassthroughCopy("assets/js/**/*.js");
 }
