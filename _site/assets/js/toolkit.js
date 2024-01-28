619a1aee2b813f7edc7ea677fff054f27c6a8b9a
Kenny <stickerboy@users.noreply.github.com>
2024-01-28 00:19:26 +0000

    Theme switcher support
    Using the js theme switcher provided on the Bootstrap website and more or less the same dropdown layout.
    
    Made one change to switch to CSS classes for the icons instead of SVGs
    
    Changed the clear data icon to recycle, works a little better on a button without borders
    
    Swapped out a lot of color specific CSS classes on elements, for CSS that targets dark mode (where appropriate)

--- _site/assets/js/toolkit.js
+++ _site/assets/js/toolkit.js
@@ -392,7 +392,7 @@ function styledUniqueArrayItems(data) {
     let result = `<div class="g-col-12"><p class="display-5 fs-5 mt-4">Unique chracters</p>
     <div class="grid mt-2 grid-auto" id="unique-chars">`;
     data.forEach(char => {
-        result += `<code class="d-inline-flex px-2 text-dark bg-success bg-opacity-10 border border-success border-opacity-10 rounded-2 me-2" style="width: max-content;" aria-label="${char.replace(/ /g, "Space")}" title="${char.replace(/ /g, "Space")}">
+        result += `<code class="d-inline-flex px-2 bg-success bg-opacity-10 border border-success border-opacity-10 rounded-2 me-2" style="width: max-content;" aria-label="${char.replace(/ /g, "Space")}" title="${char.replace(/ /g, "Space")}">
                 ${char.replace(/ /g, "&nbsp;")}
             </code>`;
     });
@@ -411,7 +411,7 @@ function styledArrayFrequencies(data) {
     let result = `<div class="g-col-12"><p class="display-5 fs-5 mt-4">Unique character frequencies</p>
     <div class="grid mt-2" id="unique-chars">`;
     for (let [key, value] of Object.entries(data)) {
-        result += `<div class="g-col-4 g-col-md-3 g-col-lg-2 g-col-xxl-1"><code class="d-inline-flex px-2 text-dark bg-success bg-opacity-10 border border-success border-opacity-10 rounded-2 me-2" style="width: max-content;" aria-label="Frequency of ${key.replace(/ /g, "Space")}" title="Frequency of ${key.replace(/ /g, "Space")}">
+        result += `<div class="g-col-4 g-col-md-3 g-col-lg-2 g-col-xxl-1"><code class="d-inline-flex px-2 bg-success bg-opacity-10 border border-success border-opacity-10 rounded-2 me-2" style="width: max-content;" aria-label="Frequency of ${key.replace(/ /g, "Space")}" title="Frequency of ${key.replace(/ /g, "Space")}">
                 ${key.replace(/ /g, "Space")} - ${value}
             </code></div>`;
     }
