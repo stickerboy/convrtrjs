4bdc0c60482dbfba85f9728298609042471d3af2
Kenny <stickerboy@users.noreply.github.com>
2024-02-11 18:21:18 +0000

    The elements!
    Added a json file from form the Bowserinator repo that contains info on all periodic elements: https://github.com/Bowserinator/Periodic-Table-JSON/
    
    Ability to perform conversions between different element properties (though only a subset have been surfaced for the moment).
    For example, you can convert a string of atomic numbers to their element names, boiling points, etc...

---
+++ assets/js/misc/misc.js
@@ -0,0 +1,64 @@
+function convertElements(string, sourceProp, targetProp, removeDelimiters) {
+    const strings = string.split(/[ ,:;\-]+/);
+    const results = [];
+
+    for (const str of strings) {
+        let found = false;
+
+        for (const el of ELEMENTS) {
+            if (el.hasOwnProperty(sourceProp)) {
+                // Compare the value regardless of its type (string or number)
+                if (el[sourceProp] === str || el["number"] === Number(str)) {
+                    if (el.hasOwnProperty(targetProp)) {
+                        const targetValue = el[targetProp];
+                        // console.log(`Converted ${sourceProp} (${str}) to ${targetProp} (${targetValue})`);
+                        results.push(targetValue);
+                        found = true;
+                        break;
+                    } else {
+                        throw new Error(`Property "${targetProp}" does not exist for ${el.name}`);
+                    }
+                }
+            }
+        }
+
+        if (!found) {
+            throw new Error(`Element with ${sourceProp} "${str}" not found`);
+        }
+    }
+
+    let convertedElements;
+    if (removeDelimiters && removeDelimiters === true) {
+        convertedElements = results.join("");
+    } else {
+        convertedElements = results.join(string.match(/[ ,\-]+/));
+    }
+    return convertedElements;
+}
+
+
+// Periodic element conversion
+const elementsChange = document.getElementById("elementsChange");
+elementsChange.addEventListener("click", function() {
+    let elementsString = document.getElementById("elementsTextarea");
+
+    if(!emptyContainerCheck(elementsString.value, elementsString)) {
+        return false;
+    }
+    if (!largeDataWarning(elementsString.value, elementsString)) {
+        return false;
+    }
+
+    let elementPropFrom = document.getElementById("elementPropFrom");
+    let elementPropTo = document.getElementById("elementPropTo");
+    let removeDelimiters = document.getElementById("removeDelimiters");
+
+    try {
+        convertElements(elementsString.value, elementPropFrom.value, elementPropTo.value);
+    } catch(e) {
+        showToast("Error", `An error occured trying to map elements: ${e.message}`, "danger");
+        return;
+    }
+
+    document.getElementById("elementResults").textContent = convertElements(elementsString.value, elementPropFrom.value, elementPropTo.value, removeDelimiters.checked);
+});
