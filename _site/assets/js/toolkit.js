89cd98e522f4b33e52975b01b62992bf6aabf761
Kenny <stickerboy@users.noreply.github.com>
2024-11-13 12:16:05 +0000

    Porting ONI.rb functions - interleave
    Interleaves the characters of two strings:
    "abc" + "123" => "a1b2c3"

--- _site/assets/js/toolkit.js
+++ _site/assets/js/toolkit.js
@@ -320,6 +320,36 @@ function replaceChars(string, toReplace, replacement, caseSensitive) {
     return string.replaceAll(new RegExp(toReplace, `g${caseSensitive === true ? "" : "i"}`), replacement);
 }
 
+
+/**
+ * Interleaves the characters of two strings.
+ *
+ * @param {string} string1 - The first string.
+ * @param {string} string2 - The second string.
+ * * @param {string} delimiter - An optional delimiter which may be useful when interleaving hexadecimal strings.
+ * @returns {string} - The interleaved string.
+ */
+function interleave(string1, string2, delimiter = "") {
+    let result = [];
+    let length = Math.min(string1.length, string2.length);
+
+    for (let i = 0; i < length; i++) {
+        result.push(string1[i], string2[i]);
+    }
+
+    // If string1 is longer, add the remaining characters
+    if (string1.length > string2.length) {
+        result.push(...string1.slice(length));
+    }
+
+    // If string2 is longer, add the remaining characters
+    if (string2.length > string1.length) {
+        result.push(...string2.slice(length));
+    }
+
+    return result.join(delimiter);
+}
+
 /**
  * Creates styled HTML elements for displaying unique characters from an array.
  *
