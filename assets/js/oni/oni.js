10211cd0954a3c68f1146b8e20ac980b9d43b4b7
Kenny <stickerboy@users.noreply.github.com>
2024-11-13 16:48:30 +0000

    Porting ONI.rb tools - calendar
    Extracts characters from the data string at specific positions based on calendar-like offsets.

--- assets/js/oni/oni.js
+++ assets/js/oni/oni.js
@@ -147,3 +147,25 @@ function strings(string, n = 4) {
     const regex = new RegExp(`[\\x20-\\x7E]{${n},}`, 'g');
     return string.match(regex) || [];
 }
+
+/**
+ * Extracts characters from the data string at specific positions based on calendar-like offsets.
+ *
+ * @param {string} string - The input string.
+ * @returns {string} - The resulting string after extracting specific characters.
+ */
+function calendar(string) {
+    const deltas = [31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365];
+    let out = "";
+    const count = string.length;
+    let offset = 31;
+    let n = 1;
+
+    while (offset < count) {
+        out += string[offset];
+        offset = deltas[n % 12] + n + Math.floor(n / 12) * 365;
+        n += 1;
+    }
+
+    return out;
+}
