f963160b7fb15838a6e2f551700fcf9eb73344a8
Kenny Cameron <stickerboy@users.noreply.github.com>
2024-11-14 15:04:55 +0000

    Throw an error if strings gets passed a key less than 1

--- assets/js/oni/oni.js
+++ assets/js/oni/oni.js
@@ -143,9 +143,13 @@ function interleave(string1, string2, delimiter = "") {
  * @returns {Array<string>} - An array of sequences of printable characters of at least length `n`.
  */
 function strings(string, n = 4) {
-    // Regular expression to match sequences of at least `n` printable characters
-    const regex = new RegExp(`[\\x20-\\x7E]{${n},}`, 'g');
-    return string.match(regex) || [];
+    if (n < 1) {
+        throw new Error("Minimum character count cannot be less than one");
+    } else {
+        // Regular expression to match sequences of at least `n` printable characters
+        const regex = new RegExp(`[\\x20-\\x7E]{${n},}`, 'g');
+        return string.match(regex) || [];
+    }
 }
 
 /**
