8231eaf5c480eae878498dce2c5e2cc47e8cdbeb
Kenny <stickerboy@users.noreply.github.com>
2024-11-18 10:31:54 +0000

    Improved dynamic include support
    Only include partials and assets that actually exist - making it easier to set up new sections.
    Set fallbacks for routes, removed redundant routes

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.ignores.add("README.md");
     eleventyConfig.setWatchThrottleWaitTime(100);
@@ -7,4 +10,13 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/fonts/*");
     eleventyConfig.addPassthroughCopy("assets/js/**/*.js");
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+    eleventyConfig.addFilter('fileExists', function(filePath) {
+        const fullPath = path.join('_includes', filePath);
+        return fs.existsSync(fullPath);
+    });
+    eleventyConfig.addFilter('assetExists', function(filePath) {
+        const fullJSPath = path.join('assets', filePath);
+        console.log(fullJSPath);
+        return fs.existsSync(fullJSPath);
+    });
 }
