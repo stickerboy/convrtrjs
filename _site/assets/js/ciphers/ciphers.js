e98a804aa36f937a3bb6bfc9fb3c96412e900b3e
Kenny <stickerboy@users.noreply.github.com>
2024-11-24 19:33:12 +0000

    Simplify has generation in the same manner as text tools

--- _site/assets/js/ciphers/ciphers.js
+++ _site/assets/js/ciphers/ciphers.js
@@ -62,46 +62,30 @@ function substituteChars(string, key) {
 }
 
 /**
- * Generates hash values using various hash algorithms
+ * Generates a hash for the given string based on the specified hash method.
  *
  * @param {string} string - The input string to be hashed.
- * @param {string} hash - The hash method (e.g., "MD5", "SHA1", etc.).
- * @param {string} key - Optional key (if applicable for the hash method).
- * @returns {string} - The hash value or an error message if the method is invalid or not supported.
+ * @param {string} hash - The hash method to be used (e.g., "MD5", "SHA1", etc.).
+ * @returns {string} - The generated hash value or an error message if the method is invalid or not supported.
  *
  * @example
  * // Input: "Hello, World!" with hash method "MD5"
  * // Output: "65a8e27d8879283831b664bd8b7f0ad4"
  */
-function generateHashes(string, hash, key) {
-    switch (hash) {
-        case "MD5": 
-            return CryptoJS.MD5(string);
-        break;
-        case "SHA1":
-            return CryptoJS.SHA1(string);
-        break;
-        case "SHA256":
-            return CryptoJS.SHA256(string);
-        break;
-        case "SHA512":
-            return CryptoJS.SHA512(string);
-        break;
-        case "SHA3512":
-            return CryptoJS.SHA3(string, { outputLength: 512 });
-        break;
-        case "SHA3384":
-            return CryptoJS.SHA3(string, { outputLength: 384 });
-        break;
-        case "SHA3256":
-            return CryptoJS.SHA3(string, { outputLength: 256 });
-        break;
-        case "SHA3224":
-            return CryptoJS.SHA3(string, { outputLength: 224 });
-        break;
-        default:
-            return "Invalid hash method provided or not supported";
-    }
+function generateHashes(string, hash) {
+    const hashFunctions = {
+        MD5: CryptoJS.MD5,
+        SHA1: CryptoJS.SHA1,
+        SHA256: CryptoJS.SHA256,
+        SHA512: CryptoJS.SHA512,
+        SHA3512: string => CryptoJS.SHA3(string, { outputLength: 512 }),
+        SHA3384: string => CryptoJS.SHA3(string, { outputLength: 384 }),
+        SHA3256: string => CryptoJS.SHA3(string, { outputLength: 256 }),
+        SHA3224: string => CryptoJS.SHA3(string, { outputLength: 224 })
+    };
+
+    const selectedHashFunction = hashFunctions[hash];
+    return selectedHashFunction ? selectedHashFunction(string).toString() : "Invalid hash method provided or not supported";
 }
 
 /**
