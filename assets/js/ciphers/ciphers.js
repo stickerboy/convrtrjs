22bb7f7a07340547540ad4d2ece336d4d013b1ba
Kenny Cameron <stickerboy@users.noreply.github.com>
2024-12-19 22:26:46 +0000

    Adding liquid template and associated frontend JS for Smithy Encrypt/Decrypt

--- assets/js/ciphers/ciphers.js
+++ assets/js/ciphers/ciphers.js
@@ -823,6 +823,53 @@ chaosDecryptButton && chaosDecryptButton.addEventListener("click", function () {
 });
 
 
+
+
+// Smithy cipher
+const smithyEncryptButton = document.getElementById("smithyEncrypt");
+smithyEncryptButton && smithyEncryptButton.addEventListener("click", function () {
+    const smithyString = document.getElementById("smithyText");
+    const smithyKey = document.getElementById("smithyKey");
+
+    if (!emptyContainerCheck(smithyString.value, smithyString)) {
+        return false;
+    }
+    if (!emptyContainerCheck(smithyKey.value, smithyKey, "A key is required to encrypt this cipher")) {
+        return false;
+    }
+    if (!largeDataWarning(smithyString.value, smithyString)) {
+        return false;
+    }
+
+    let chainSm = document.getElementById("chainSm");
+    let sM = document.getElementById("smithyResults").textContent;
+    let kwResults = chainSm.checked && sM.length > 0 ? sM : smithyString.value;
+
+    document.getElementById("smithyResults").textContent = smithyEncrypt(kwResults, smithyKey.value);
+});
+const smithyDecryptButton = document.getElementById("smithyDecrypt");
+smithyDecryptButton && smithyDecryptButton.addEventListener("click", function () {
+    const smithyString = document.getElementById("smithyText");
+    const smithyKey = document.getElementById("smithyKey");
+
+    if (!emptyContainerCheck(smithyString.value, smithyString)) {
+        return false;
+    }
+    if (!emptyContainerCheck(smithyKey.value, smithyKey, "A key is required to decrypt this cipher")) {
+        return false;
+    }
+    if (!largeDataWarning(smithyString.value, smithyString)) {
+        return false;
+    }
+
+    let chainSm = document.getElementById("chainSm");
+    let sM = document.getElementById("smithyResults").textContent;
+    let kwResults = chainSm.checked && sM.length > 0 ? sM : smithyString.value;
+
+    document.getElementById("smithyResults").textContent = smithyDecrypt(kwResults, smithyKey.value);
+});
+
+
 // Atbash cipher
 const atbashDecodeButton = document.getElementById("atbashDecode");
 atbashDecodeButton && atbashDecodeButton.addEventListener("click", function() {
