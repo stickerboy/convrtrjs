901268188ba34129e563d9ffe54d198cc40a2305
Kenny Cameron <stickerboy@users.noreply.github.com>
2025-01-03 12:21:54 +0000

    Fix incorrect reference to hexToString function

--- assets/js/home/convrtrs.mjs
+++ assets/js/home/convrtrs.mjs
@@ -76,20 +76,20 @@ hexDecodeButton && hexDecodeButton.addEventListener("click", function() {
     }
 
     try {
-        home.hexToString(data, hexDelimiter);
+        toolkit.hexToString(data, hexDelimiter);
     } catch (e) {
         showToast("Error", `An error occurred trying to decode the data: ${e.message}`, "danger");
         return;
     }
 
-    document.getElementById("convrtrs-text").querySelector("textarea").value = home.hexToString(data, hexDelimiter);
-    document.getElementById("convrtrs-binary").querySelector("textarea").value = toolkit.stringToBinary(home.hexToString(data, hexDelimiter));
-    document.getElementById("convrtrs-base64").querySelector("textarea").value = toolkit.stringToBase64(home.hexToString(data, hexDelimiter));
-    document.getElementById("convrtrs-decimal").querySelector("textarea").value = toolkit.stringToDecimal(home.hexToString(data, hexDelimiter));
+    document.getElementById("convrtrs-text").querySelector("textarea").value = toolkit.hexToString(data, hexDelimiter);
+    document.getElementById("convrtrs-binary").querySelector("textarea").value = toolkit.stringToBinary(toolkit.hexToString(data, hexDelimiter));
+    document.getElementById("convrtrs-base64").querySelector("textarea").value = toolkit.stringToBase64(toolkit.hexToString(data, hexDelimiter));
+    document.getElementById("convrtrs-decimal").querySelector("textarea").value = toolkit.stringToDecimal(toolkit.hexToString(data, hexDelimiter));
     document.getElementById("convrtrs-reverse").querySelector("textarea").value = toolkit.reverseString(data);
     document.getElementById("convrtrs-rot13").querySelector("textarea").value = rot13(data);
-    document.getElementById("convrtrs-morse").querySelector("textarea").value = home.stringToMorse(home.hexToString(data, hexDelimiter));
-    document.getElementById("convrtrs-morsenary").querySelector("textarea").value = home.stringToMorsenary(home.hexToString(data, hexDelimiter));
+    document.getElementById("convrtrs-morse").querySelector("textarea").value = home.stringToMorse(toolkit.hexToString(data, hexDelimiter));
+    document.getElementById("convrtrs-morsenary").querySelector("textarea").value = home.stringToMorsenary(toolkit.hexToString(data, hexDelimiter));
 });
 
 
