422d692eafb36c76d972cd532aa114081c3c937a
Kenny Cameron <stickerboy@users.noreply.github.com>
2025-09-17 10:56:55 +0100

    Configuration changes for search
    Updated some config settings:
    - default returned results to 10
    - increased excerpt length
    
    Removed some text elements from the index: some toggle switches, dropdowns, pagination, buttons, etc
    Excluded changelog tag pages from search results, as they're already covered by the main changelog page
    Attempted to reduce the weight of changelog items, in favor of all the other pages

--- assets/js/search.mjs
+++ assets/js/search.mjs
@@ -1,3 +1,10 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    new PagefindUI({ element: "#search", showSubResults: true, showImages: false, resetStyles: false });
+    new PagefindUI({ 
+        element: "#search", 
+        showSubResults: true, 
+        showImages: false, 
+        resetStyles: false,
+        pageSize: 10,
+        excerptLength: 42
+    });
 });
