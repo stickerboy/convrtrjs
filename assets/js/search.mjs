96f0b77da2aedb29b84c1bdfe2b637b02fb6dcc6
Kenny Cameron <stickerboy@users.noreply.github.com>
2025-09-17 16:04:24 +0100

    Search filters, custom styling and style overrides
    Enabled tag/filter support in search
    Added some initial tags to changelogs/tags
    Set up some custom styling to make the results look a little closer to site design, bringing in bootstrap classes
    Added custom JS observer to force tags/filters to be open once focus is gained on the input

--- assets/js/search.mjs
+++ assets/js/search.mjs
@@ -7,4 +7,37 @@ window.addEventListener('DOMContentLoaded', (event) => {
         pageSize: 10,
         excerptLength: 42
     });
+
+    const searchInput = document.querySelector(".pagefind-ui__search-input");
+    const searchFilter = document.querySelector('.pagefind-ui__drawer');
+
+    function openDetails() {
+        const details = searchFilter.querySelector('details'); 
+        if (details && !details.open) {
+            details.open = true;
+        }
+    }
+
+    // Run observer as search filters are injected dynamically on input focus
+    function observeDetails() {
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.addedNodes.length) {
+                    mutation.addedNodes.forEach((node) => {
+                        if (node.nodeType === Node.ELEMENT_NODE && node.matches('details')) {
+                            openDetails();
+                        }
+                    });
+                }
+            });
+        });
+
+        observer.observe(searchFilter, { childList: true, subtree: true });
+    }
+
+    observeDetails();
+
+    searchInput.addEventListener('focus', () => {
+        openDetails();
+    }, { once: true }); // Only trigger once on first focus
 });
