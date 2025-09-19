2ad6566eaff7750c7041900f3dfb7828ac71ee24
Kenny Cameron <stickerboy@users.noreply.github.com>
2025-09-19 20:49:43 +0100

    Updated search configuration
    Require a minimum of 2 characters before a search is processed. It sill still return single character results, but that can be dealt with later.
    Might look into using query strings, so i'm going to leave the history replace commented so I don't forget.
    Updated default text for things like placeholders and search result strings

--- assets/js/search.mjs
+++ assets/js/search.mjs
@@ -11,9 +11,28 @@ window.addEventListener('DOMContentLoaded', () => {
         debounceTimeoutMs: 500,
         translations: {
             placeholder: "Search convrtrs, tools, and other resources...",
+            clear_search: "Clear",
+            load_more: "Load more results",
+            search_label: "Search this site",
+            filters_label: "Filters",
+            zero_results: "There are no result for your search term: [SEARCH_TERM]",
+            many_results: "There are [COUNT] results for your search term: [SEARCH_TERM]",
+            one_result: "There is [COUNT] result for your search term: [SEARCH_TERM]",
+            alt_search: "There are no result for your search term: [SEARCH_TERM]. Showing results for [DIFFERENT_TERM] instead",
+            search_suggestion: "There are No results for your search term: [SEARCH_TERM]. Try one of the following searches:",
+            searching: "Searching for [SEARCH_TERM]..."
         },
         processResult: function (result) {
             console.log(result);
+        },
+        processTerm: function (term) {
+            term = String(term).trim();
+
+            if (term.length < 2) {
+                return "";
+            }
+            // history.replaceState({}, "", `?q=${encodeURIComponent(term)}`);
+            return term;
         }
     });
 });
